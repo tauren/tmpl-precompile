18d98cd62cbeaaecc5f63efc55dfc5781ad884ac
Grey <conancat@gmail.com>
2011-06-30 19:42:36 +0800

    merged namespacing module

--- examples/output/templates.js
+++ examples/output/templates.js
@@ -1,3 +1,10 @@
+NS = NS || {};
+NS.templates = NS.templates || {};
+NS.templates.level1 = NS.templates.level1 || {};
+NS.templates.level1.level2 = NS.templates.level1.level2 || {};
+NS.templates.level1 = NS.templates.level1 || {};
+NS.templates.level1.level2 = NS.templates.level1.level2 || {};
+NS.templates.level1.level2.level3 = NS.templates.level1.level2.level3 || {};
 NS.templates.layout = function anonymous(locals) {
 var __ = { lineno: 1, input: "!!! 5\nhtml\n\thead\n\t\ttitle\n\tbody\n\t\th1 Content goes here\n\t\t#container!= body\n", filename: undefined };
 function rethrow(err, str, filename, lineno){
@@ -92,7 +99,230 @@ buf.push('</html>');}return buf.join("");
 } catch (err) {
   rethrow(err, __.input, __.filename, __.lineno);
 }
-};NS.templates.root = function anonymous(locals) {
+};
+NS.templates.root = function anonymous(locals) {
+var __ = { lineno: 1, input: "h2 Hello\np World!", filename: undefined };
+function rethrow(err, str, filename, lineno){
+  var context = 3
+    , lines = str.split('\n')
+    , start = Math.max(lineno - context, 0)
+    , end = Math.min(lines.length, lineno + context); 
+
+  // Error context
+  var context = lines.slice(start, end).map(function(line, i){
+    var curr = i + start + 1;
+    return (curr == lineno ? '  > ' : '    ')
+      + curr
+      + '| '
+      + line;
+  }).join('\n');
+
+  // Alter exception message
+  err.path = filename;
+  err.message = (filename || 'Jade') + ':' + lineno 
+    + '\n' + context + '\n\n' + err.message;
+  throw err;
+}
+try {
+function attrs(obj){
+  var buf = []
+    , terse = obj.terse;
+  delete obj.terse;
+  var keys = Object.keys(obj)
+    , len = keys.length;
+  if (len) {
+    buf.push('');
+    for (var i = 0; i < len; ++i) {
+      var key = keys[i]
+        , val = obj[key];
+      if ('boolean' == typeof val || null == val) {
+        if (val) {
+          terse
+            ? buf.push(key)
+            : buf.push(key + '="' + key + '"');
+        }
+      } else {
+        buf.push(key + '="' + escape(val) + '"');
+      }
+    }
+  }
+  return buf.join(' ');
+}
+
+function escape(html){
+  return String(html)
+    .replace(/&(?!\w+;)/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;');
+}
+
+var buf = [];
+with (locals || {}) {var interp;
+__.lineno = 1;
+__.lineno = 1;
+buf.push('<h2>');
+buf.push('Hello');
+__.lineno = undefined;
+buf.push('</h2>');
+__.lineno = 2;
+buf.push('<p>');
+buf.push('World!');
+__.lineno = undefined;
+buf.push('</p>');}return buf.join("");
+} catch (err) {
+  rethrow(err, __.input, __.filename, __.lineno);
+}
+};
+NS.templates.level1.root = function anonymous(locals) {
+var __ = { lineno: 1, input: "h2 Hello\np World!", filename: undefined };
+function rethrow(err, str, filename, lineno){
+  var context = 3
+    , lines = str.split('\n')
+    , start = Math.max(lineno - context, 0)
+    , end = Math.min(lines.length, lineno + context); 
+
+  // Error context
+  var context = lines.slice(start, end).map(function(line, i){
+    var curr = i + start + 1;
+    return (curr == lineno ? '  > ' : '    ')
+      + curr
+      + '| '
+      + line;
+  }).join('\n');
+
+  // Alter exception message
+  err.path = filename;
+  err.message = (filename || 'Jade') + ':' + lineno 
+    + '\n' + context + '\n\n' + err.message;
+  throw err;
+}
+try {
+function attrs(obj){
+  var buf = []
+    , terse = obj.terse;
+  delete obj.terse;
+  var keys = Object.keys(obj)
+    , len = keys.length;
+  if (len) {
+    buf.push('');
+    for (var i = 0; i < len; ++i) {
+      var key = keys[i]
+        , val = obj[key];
+      if ('boolean' == typeof val || null == val) {
+        if (val) {
+          terse
+            ? buf.push(key)
+            : buf.push(key + '="' + key + '"');
+        }
+      } else {
+        buf.push(key + '="' + escape(val) + '"');
+      }
+    }
+  }
+  return buf.join(' ');
+}
+
+function escape(html){
+  return String(html)
+    .replace(/&(?!\w+;)/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;');
+}
+
+var buf = [];
+with (locals || {}) {var interp;
+__.lineno = 1;
+__.lineno = 1;
+buf.push('<h2>');
+buf.push('Hello');
+__.lineno = undefined;
+buf.push('</h2>');
+__.lineno = 2;
+buf.push('<p>');
+buf.push('World!');
+__.lineno = undefined;
+buf.push('</p>');}return buf.join("");
+} catch (err) {
+  rethrow(err, __.input, __.filename, __.lineno);
+}
+};
+NS.templates.level1.level2.root = function anonymous(locals) {
+var __ = { lineno: 1, input: "h2 Hello\np World!", filename: undefined };
+function rethrow(err, str, filename, lineno){
+  var context = 3
+    , lines = str.split('\n')
+    , start = Math.max(lineno - context, 0)
+    , end = Math.min(lines.length, lineno + context); 
+
+  // Error context
+  var context = lines.slice(start, end).map(function(line, i){
+    var curr = i + start + 1;
+    return (curr == lineno ? '  > ' : '    ')
+      + curr
+      + '| '
+      + line;
+  }).join('\n');
+
+  // Alter exception message
+  err.path = filename;
+  err.message = (filename || 'Jade') + ':' + lineno 
+    + '\n' + context + '\n\n' + err.message;
+  throw err;
+}
+try {
+function attrs(obj){
+  var buf = []
+    , terse = obj.terse;
+  delete obj.terse;
+  var keys = Object.keys(obj)
+    , len = keys.length;
+  if (len) {
+    buf.push('');
+    for (var i = 0; i < len; ++i) {
+      var key = keys[i]
+        , val = obj[key];
+      if ('boolean' == typeof val || null == val) {
+        if (val) {
+          terse
+            ? buf.push(key)
+            : buf.push(key + '="' + key + '"');
+        }
+      } else {
+        buf.push(key + '="' + escape(val) + '"');
+      }
+    }
+  }
+  return buf.join(' ');
+}
+
+function escape(html){
+  return String(html)
+    .replace(/&(?!\w+;)/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;');
+}
+
+var buf = [];
+with (locals || {}) {var interp;
+__.lineno = 1;
+__.lineno = 1;
+buf.push('<h2>');
+buf.push('Hello');
+__.lineno = undefined;
+buf.push('</h2>');
+__.lineno = 2;
+buf.push('<p>');
+buf.push('World!');
+__.lineno = undefined;
+buf.push('</p>');}return buf.join("");
+} catch (err) {
+  rethrow(err, __.input, __.filename, __.lineno);
+}
+};
+NS.templates.level1.level2.level3.root = function anonymous(locals) {
 var __ = { lineno: 1, input: "h2 Hello\np World!", filename: undefined };
 function rethrow(err, str, filename, lineno){
   var context = 3
--- lib/tmpl-precompile.coffee
+++ lib/tmpl-precompile.coffee
@@ -1,36 +1,139 @@
 version = [0,1,0]
 
+jade = require('jade')
 fs = require 'fs'
-jade = require 'jade'
 jsp = require("uglify-js").parser
 pro = require("uglify-js").uglify
 cwd = ''
 
 compileTemplate = (template, group) ->
-	console.log 'Compiling '+ group.namespace + '.' + template + ' from ' + 
-		group.source + template + '.jade'
-	data = fs.readFileSync cwd + group.source + template + '.jade', 'utf8'
-	source = group.namespace + '.' + template + ' = ' + jade.compile(data) + ';'
-
+  templateNamespace = template.replace(/\//g, '.') # Replaces '/' with '.'
+  data = fs.readFileSync(cwd + group.source + template + '.jade', 'utf8')
+  source = group.namespace + '.' + templateNamespace + ' = ' + jade.compile(data) + ';\n'
+  console.log "Compiling #{group.namespace}.#{templateNamespace} from #{group.source+template}"	
+  source
+  
 uglifyOutput = (output) ->
 	ast = jsp.parse output  # parse code and get the initial AST
 	ast = pro.ast_mangle ast # get a new AST with mangled names
 	ast = pro.ast_squeeze ast # get an AST with compression optimizations
 	pro.gen_code ast # compressed code here
- 
+
+
+# splitNamespace(string, namespaces, callback, isGroupNamespace)
+# Description: Splits each string and adds it into the 'namespaces' array for later processing
+# 
+# string: Namespace name
+# namespaces: Array to hold all namespaces
+# callback: function to perform after done splitting
+# isGroupNamespace: boolean value to determine if the current iteration is for group
+
+splitNamespace = (string, namespaces, callback, isGroupNamespace) ->
+  arr = string.split(/\.|\//) # Split template into array
+  
+  # Determine the maximum depth to compile
+  if isGroupNamespace then max = arr.length else max = arr.length - 1
+
+  namespaces.push arr[0] # Push base namespace
+  
+  str = arr[0] # Set base namespace
+  
+  for i in [1...max]
+    str += ".#{arr[i]}" # Appends additional levels of namespacing
+    namespaces.push str
+
+  if callback? then callback(namespaces) # Return namespaces array
+  
+  
+
+# checkGroupNamespace(group, namespaces)
+# Description: Checks group namespace if needed to be splitted
+# 
+# group: The 'group' object to process
+# namespaces: Array to hold all namespaces
+checkGroupNamespace = (group, namespaces) ->
+  callback = (namespaces) ->
+    checkTemplateNamespaces(group, namespaces)
+  
+  if group.namespace.indexOf('.') > 0 # Checks if needed to split namespace
+    splitNamespace(group.namespace, namespaces, callback, true)
+  else
+    namespaces.push(group.namespace)
+    callback(namespaces)
+
+
+
+# checkTemplateNamespaces(group, namespaces)
+# Description: Checks template namespaces if needed to be splitted
+# 
+# group: The 'group' object to process
+# namespaces: Array to hold all namespaces
+checkTemplateNamespaces = (group, namespaces) ->
+  counter = 0
+  callback = (namespaces) ->
+    counter++ # Counter to determine when iteration is done
+    if counter is group.templates.length
+      if namespaces.length > 0 then createNamespaces(group, namespaces) 
+      else compile(group, namespaces)
+  
+  # Iterate through the templates
+  for template in group.templates
+    if template.indexOf('/') > 0  # Checks if needed to split namespace
+      splitNamespace(template, namespaces, callback)
+    else callback(namespaces)
+
+
+
+# createNamespaces(group, namespaces)
+# Description: Prepends required namespace declarations for the browser
+# 
+# group: The 'group' object to process
+# namespaces: Array to hold all namespaces
+createNamespaces = (group, namespaces) ->
+  nameSpaceBuf = ''
+  
+  # Get the maximum index for group namespaces in the namespaces array
+  groupNamespaceLength = (group.namespace).split('.').length  
+  
+  # Callback
+  next = ->
+    namespaces = nameSpaceBuf # Converts the array object into a string to pass to compile()
+    compile(group, namespaces)
+  
+  # Appends the group namespace declarations
+  for g in [0...groupNamespaceLength]
+    nameSpaceBuf += "#{namespaces[g]} = #{namespaces[g]} || {};\n"
+    
+  # Appends the template namespace declarations
+  if groupNamespaceLength is namespaces.length
+    next()
+  else 
+    for t in [groupNamespaceLength...namespaces.length]
+      if namespaces[t] isnt namespaces[t+1] # Check to remove duplicates
+        nameSpaceBuf += "#{group.namespace}.#{namespaces[t]} = #{group.namespace}.#{namespaces[t]} || {};\n"
+      if t is namespaces.length-1
+        next()
+
+
+
+# compile(group, namespaces)
+# Description: Starts compiling the templates and outputs file
+# 
+# group: The 'group' object to process
+# namespaces: Buffer string, the result of namespace checking
+compile = (group, namespaces) ->
+  buf = namespaces || '' # Prepends namespaces declarations
+  for template in group.templates
+	  buf += compileTemplate(template, group).toString()
+  buf = uglifyOutput buf if group.uglify
+  console.log 'Saving '+ group.output
+  fs.writeFileSync cwd + group.output, buf
+
 buildGroup = (group) ->
-	buf = ''
-	for template in group.templates
-		buf += compileTemplate(template, group).toString() 
-	if group.uglify
-		console.log 'Saving and Uglifying '+ group.output
-		buf = uglifyOutput buf 
-	else
-		console.log 'Saving '+ group.output
-	fs.writeFileSync cwd + group.output, buf
+  namespaces = []
+  checkGroupNamespace(group, namespaces)
 
 exports.precompile = (settings,dir) ->
-	console.log(settings)
 	cwd = dir
 	for group in settings.groups
 		buildGroup group
