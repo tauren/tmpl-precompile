214394d3fba4bdbf5898d696384fb992ae49546e
Grey <conancat@gmail.com>
2011-07-16 14:28:23 +0800

    using jade.helpers for adding helpers to file

--- examples/output/single-namespace.js
+++ examples/output/single-namespace.js
@@ -1,28 +1,3 @@
-singleNamespace = singleNamespace || {};
-singleNamespace.layout = function anonymous(locals) {
-var __ = { lineno: 1, input: "!!! 5\nhtml\n\thead\n\t\ttitle\n\tbody\n\t\th1 Content goes here\n\t\t#container!= body\n", filename: undefined };
-function rethrow(err, str, filename, lineno){
-  var context = 3
-    , lines = str.split('\n')
-    , start = Math.max(lineno - context, 0)
-    , end = Math.min(lines.length, lineno + context); 
-
-  // Error context
-  var context = lines.slice(start, end).map(function(line, i){
-    var curr = i + start + 1;
-    return (curr == lineno ? '  > ' : '    ')
-      + curr
-      + '| '
-      + line;
-  }).join('\n');
-
-  // Alter exception message
-  err.path = filename;
-  err.message = (filename || 'Jade') + ':' + lineno 
-    + '\n' + context + '\n\n' + err.message;
-  throw err;
-}
-try {
 function attrs(obj){
   var buf = []
     , terse = obj.terse;
@@ -49,7 +24,6 @@ function attrs(obj){
   }
   return buf.join(' ');
 }
-
 function escape(html){
   return String(html)
     .replace(/&(?!\w+;)/g, '&amp;')
@@ -57,68 +31,67 @@ function escape(html){
     .replace(/>/g, '&gt;')
     .replace(/"/g, '&quot;');
 }
-
+ var jadeHelpers = {attrs: attrs, escape: escape}; 
+singleNamespace = singleNamespace || {};
+singleNamespace.layout = function anonymous(locals) {
+function attrs(obj){
+  var buf = []
+    , terse = obj.terse;
+  delete obj.terse;
+  var keys = Object.keys(obj)
+    , len = keys.length;
+  if (len) {
+    buf.push('');
+    for (var i = 0; i < len; ++i) {
+      var key = keys[i]
+        , val = obj[key];
+      if ('boolean' == typeof val || null == val) {
+        if (val) {
+          terse
+            ? buf.push(key)
+            : buf.push(key + '="' + key + '"');
+        }
+      } else if ('class' == key && Array.isArray(val)) {
+        buf.push(key + '="' + escape(val.join(' ')) + '"');
+      } else {
+        buf.push(key + '="' + escape(val) + '"');
+      }
+    }
+  }
+  return buf.join(' ');
+}
+function escape(html){
+  return String(html)
+    .replace(/&(?!\w+;)/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;');
+}
 var buf = [];
-with (locals || {}) {__.lineno = 1;
-__.lineno = 1;
+with (locals || {}) {
+var interp;
 buf.push('<!DOCTYPE html>');
-__.lineno = 3;
 buf.push('<html>');
-__.lineno = undefined;
-__.lineno = 4;
 buf.push('<head>');
-__.lineno = undefined;
-__.lineno = 5;
 buf.push('<title>');
-__.lineno = undefined;
 buf.push('</title>');
 buf.push('</head>');
-__.lineno = 6;
 buf.push('<body>');
-__.lineno = undefined;
-__.lineno = 6;
 buf.push('<h1>');
 buf.push('Content goes here');
-__.lineno = undefined;
 buf.push('</h1>');
-__.lineno = 7;
 buf.push('<div');
 buf.push(attrs({ terse: true, 'id':('container') }));
 buf.push('>');
 var __val__ = body
 buf.push(null == __val__ ? "" : __val__);
-__.lineno = undefined;
 buf.push('</div>');
 buf.push('</body>');
-buf.push('</html>');}return buf.join("");
-} catch (err) {
-  rethrow(err, __.input, __.filename, __.lineno);
+buf.push('</html>');
 }
+return buf.join("");
 };
 singleNamespace.root = function anonymous(locals) {
-var __ = { lineno: 1, input: "h2 Hello\np World!", filename: undefined };
-function rethrow(err, str, filename, lineno){
-  var context = 3
-    , lines = str.split('\n')
-    , start = Math.max(lineno - context, 0)
-    , end = Math.min(lines.length, lineno + context); 
-
-  // Error context
-  var context = lines.slice(start, end).map(function(line, i){
-    var curr = i + start + 1;
-    return (curr == lineno ? '  > ' : '    ')
-      + curr
-      + '| '
-      + line;
-  }).join('\n');
-
-  // Alter exception message
-  err.path = filename;
-  err.message = (filename || 'Jade') + ':' + lineno 
-    + '\n' + context + '\n\n' + err.message;
-  throw err;
-}
-try {
 function attrs(obj){
   var buf = []
     , terse = obj.terse;
@@ -145,7 +118,6 @@ function attrs(obj){
   }
   return buf.join(' ');
 }
-
 function escape(html){
   return String(html)
     .replace(/&(?!\w+;)/g, '&amp;')
@@ -153,20 +125,15 @@ function escape(html){
     .replace(/>/g, '&gt;')
     .replace(/"/g, '&quot;');
 }
-
 var buf = [];
-with (locals || {}) {__.lineno = 1;
-__.lineno = 1;
+with (locals || {}) {
+var interp;
 buf.push('<h2>');
 buf.push('Hello');
-__.lineno = undefined;
 buf.push('</h2>');
-__.lineno = 2;
 buf.push('<p>');
 buf.push('World!');
-__.lineno = undefined;
-buf.push('</p>');}return buf.join("");
-} catch (err) {
-  rethrow(err, __.input, __.filename, __.lineno);
+buf.push('</p>');
 }
+return buf.join("");
 };
--- examples/output/templates.js
+++ examples/output/templates.js
@@ -1,3 +1,37 @@
+function attrs(obj){
+  var buf = []
+    , terse = obj.terse;
+  delete obj.terse;
+  var keys = Object.keys(obj)
+    , len = keys.length;
+  if (len) {
+    buf.push('');
+    for (var i = 0; i < len; ++i) {
+      var key = keys[i]
+        , val = obj[key];
+      if ('boolean' == typeof val || null == val) {
+        if (val) {
+          terse
+            ? buf.push(key)
+            : buf.push(key + '="' + key + '"');
+        }
+      } else if ('class' == key && Array.isArray(val)) {
+        buf.push(key + '="' + escape(val.join(' ')) + '"');
+      } else {
+        buf.push(key + '="' + escape(val) + '"');
+      }
+    }
+  }
+  return buf.join(' ');
+}
+function escape(html){
+  return String(html)
+    .replace(/&(?!\w+;)/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;');
+}
+ var jadeHelpers = {attrs: attrs, escape: escape}; 
 NS = NS || {};
 NS.templates = NS.templates || {};
 NS.templates.level1 = NS.templates.level1 || {};
@@ -6,29 +40,6 @@ NS.templates.level1 = NS.templates.level1 || {};
 NS.templates.level1.level2 = NS.templates.level1.level2 || {};
 NS.templates.level1.level2.level3 = NS.templates.level1.level2.level3 || {};
 NS.templates.layout = function anonymous(locals) {
-var __ = { lineno: 1, input: "!!! 5\nhtml\n\thead\n\t\ttitle\n\tbody\n\t\th1 Content goes here\n\t\t#container!= body\n", filename: undefined };
-function rethrow(err, str, filename, lineno){
-  var context = 3
-    , lines = str.split('\n')
-    , start = Math.max(lineno - context, 0)
-    , end = Math.min(lines.length, lineno + context); 
-
-  // Error context
-  var context = lines.slice(start, end).map(function(line, i){
-    var curr = i + start + 1;
-    return (curr == lineno ? '  > ' : '    ')
-      + curr
-      + '| '
-      + line;
-  }).join('\n');
-
-  // Alter exception message
-  err.path = filename;
-  err.message = (filename || 'Jade') + ':' + lineno 
-    + '\n' + context + '\n\n' + err.message;
-  throw err;
-}
-try {
 function attrs(obj){
   var buf = []
     , terse = obj.terse;
@@ -55,7 +66,6 @@ function attrs(obj){
   }
   return buf.join(' ');
 }
-
 function escape(html){
   return String(html)
     .replace(/&(?!\w+;)/g, '&amp;')
@@ -63,68 +73,31 @@ function escape(html){
     .replace(/>/g, '&gt;')
     .replace(/"/g, '&quot;');
 }
-
 var buf = [];
-with (locals || {}) {__.lineno = 1;
-__.lineno = 1;
+with (locals || {}) {
+var interp;
 buf.push('<!DOCTYPE html>');
-__.lineno = 3;
 buf.push('<html>');
-__.lineno = undefined;
-__.lineno = 4;
 buf.push('<head>');
-__.lineno = undefined;
-__.lineno = 5;
 buf.push('<title>');
-__.lineno = undefined;
 buf.push('</title>');
 buf.push('</head>');
-__.lineno = 6;
 buf.push('<body>');
-__.lineno = undefined;
-__.lineno = 6;
 buf.push('<h1>');
 buf.push('Content goes here');
-__.lineno = undefined;
 buf.push('</h1>');
-__.lineno = 7;
 buf.push('<div');
 buf.push(attrs({ terse: true, 'id':('container') }));
 buf.push('>');
 var __val__ = body
 buf.push(null == __val__ ? "" : __val__);
-__.lineno = undefined;
 buf.push('</div>');
 buf.push('</body>');
-buf.push('</html>');}return buf.join("");
-} catch (err) {
-  rethrow(err, __.input, __.filename, __.lineno);
+buf.push('</html>');
 }
+return buf.join("");
 };
 NS.templates.root = function anonymous(locals) {
-var __ = { lineno: 1, input: "h2 Hello\np World!", filename: undefined };
-function rethrow(err, str, filename, lineno){
-  var context = 3
-    , lines = str.split('\n')
-    , start = Math.max(lineno - context, 0)
-    , end = Math.min(lines.length, lineno + context); 
-
-  // Error context
-  var context = lines.slice(start, end).map(function(line, i){
-    var curr = i + start + 1;
-    return (curr == lineno ? '  > ' : '    ')
-      + curr
-      + '| '
-      + line;
-  }).join('\n');
-
-  // Alter exception message
-  err.path = filename;
-  err.message = (filename || 'Jade') + ':' + lineno 
-    + '\n' + context + '\n\n' + err.message;
-  throw err;
-}
-try {
 function attrs(obj){
   var buf = []
     , terse = obj.terse;
@@ -151,7 +124,6 @@ function attrs(obj){
   }
   return buf.join(' ');
 }
-
 function escape(html){
   return String(html)
     .replace(/&(?!\w+;)/g, '&amp;')
@@ -159,47 +131,19 @@ function escape(html){
     .replace(/>/g, '&gt;')
     .replace(/"/g, '&quot;');
 }
-
 var buf = [];
-with (locals || {}) {__.lineno = 1;
-__.lineno = 1;
+with (locals || {}) {
+var interp;
 buf.push('<h2>');
 buf.push('Hello');
-__.lineno = undefined;
 buf.push('</h2>');
-__.lineno = 2;
 buf.push('<p>');
 buf.push('World!');
-__.lineno = undefined;
-buf.push('</p>');}return buf.join("");
-} catch (err) {
-  rethrow(err, __.input, __.filename, __.lineno);
+buf.push('</p>');
 }
+return buf.join("");
 };
 NS.templates.level1.root = function anonymous(locals) {
-var __ = { lineno: 1, input: "h2 Hello\np World!", filename: undefined };
-function rethrow(err, str, filename, lineno){
-  var context = 3
-    , lines = str.split('\n')
-    , start = Math.max(lineno - context, 0)
-    , end = Math.min(lines.length, lineno + context); 
-
-  // Error context
-  var context = lines.slice(start, end).map(function(line, i){
-    var curr = i + start + 1;
-    return (curr == lineno ? '  > ' : '    ')
-      + curr
-      + '| '
-      + line;
-  }).join('\n');
-
-  // Alter exception message
-  err.path = filename;
-  err.message = (filename || 'Jade') + ':' + lineno 
-    + '\n' + context + '\n\n' + err.message;
-  throw err;
-}
-try {
 function attrs(obj){
   var buf = []
     , terse = obj.terse;
@@ -226,7 +170,6 @@ function attrs(obj){
   }
   return buf.join(' ');
 }
-
 function escape(html){
   return String(html)
     .replace(/&(?!\w+;)/g, '&amp;')
@@ -234,47 +177,19 @@ function escape(html){
     .replace(/>/g, '&gt;')
     .replace(/"/g, '&quot;');
 }
-
 var buf = [];
-with (locals || {}) {__.lineno = 1;
-__.lineno = 1;
+with (locals || {}) {
+var interp;
 buf.push('<h2>');
 buf.push('Hello');
-__.lineno = undefined;
 buf.push('</h2>');
-__.lineno = 2;
 buf.push('<p>');
 buf.push('World!');
-__.lineno = undefined;
-buf.push('</p>');}return buf.join("");
-} catch (err) {
-  rethrow(err, __.input, __.filename, __.lineno);
+buf.push('</p>');
 }
+return buf.join("");
 };
 NS.templates.level1.level2.root = function anonymous(locals) {
-var __ = { lineno: 1, input: "h2 Hello\np World!", filename: undefined };
-function rethrow(err, str, filename, lineno){
-  var context = 3
-    , lines = str.split('\n')
-    , start = Math.max(lineno - context, 0)
-    , end = Math.min(lines.length, lineno + context); 
-
-  // Error context
-  var context = lines.slice(start, end).map(function(line, i){
-    var curr = i + start + 1;
-    return (curr == lineno ? '  > ' : '    ')
-      + curr
-      + '| '
-      + line;
-  }).join('\n');
-
-  // Alter exception message
-  err.path = filename;
-  err.message = (filename || 'Jade') + ':' + lineno 
-    + '\n' + context + '\n\n' + err.message;
-  throw err;
-}
-try {
 function attrs(obj){
   var buf = []
     , terse = obj.terse;
@@ -301,7 +216,6 @@ function attrs(obj){
   }
   return buf.join(' ');
 }
-
 function escape(html){
   return String(html)
     .replace(/&(?!\w+;)/g, '&amp;')
@@ -309,47 +223,19 @@ function escape(html){
     .replace(/>/g, '&gt;')
     .replace(/"/g, '&quot;');
 }
-
 var buf = [];
-with (locals || {}) {__.lineno = 1;
-__.lineno = 1;
+with (locals || {}) {
+var interp;
 buf.push('<h2>');
 buf.push('Hello');
-__.lineno = undefined;
 buf.push('</h2>');
-__.lineno = 2;
 buf.push('<p>');
 buf.push('World!');
-__.lineno = undefined;
-buf.push('</p>');}return buf.join("");
-} catch (err) {
-  rethrow(err, __.input, __.filename, __.lineno);
+buf.push('</p>');
 }
+return buf.join("");
 };
 NS.templates.level1.level2.level3.root = function anonymous(locals) {
-var __ = { lineno: 1, input: "h2 Hello\np World!", filename: undefined };
-function rethrow(err, str, filename, lineno){
-  var context = 3
-    , lines = str.split('\n')
-    , start = Math.max(lineno - context, 0)
-    , end = Math.min(lines.length, lineno + context); 
-
-  // Error context
-  var context = lines.slice(start, end).map(function(line, i){
-    var curr = i + start + 1;
-    return (curr == lineno ? '  > ' : '    ')
-      + curr
-      + '| '
-      + line;
-  }).join('\n');
-
-  // Alter exception message
-  err.path = filename;
-  err.message = (filename || 'Jade') + ':' + lineno 
-    + '\n' + context + '\n\n' + err.message;
-  throw err;
-}
-try {
 function attrs(obj){
   var buf = []
     , terse = obj.terse;
@@ -376,7 +262,6 @@ function attrs(obj){
   }
   return buf.join(' ');
 }
-
 function escape(html){
   return String(html)
     .replace(/&(?!\w+;)/g, '&amp;')
@@ -384,20 +269,15 @@ function escape(html){
     .replace(/>/g, '&gt;')
     .replace(/"/g, '&quot;');
 }
-
 var buf = [];
-with (locals || {}) {__.lineno = 1;
-__.lineno = 1;
+with (locals || {}) {
+var interp;
 buf.push('<h2>');
 buf.push('Hello');
-__.lineno = undefined;
 buf.push('</h2>');
-__.lineno = 2;
 buf.push('<p>');
 buf.push('World!');
-__.lineno = undefined;
-buf.push('</p>');}return buf.join("");
-} catch (err) {
-  rethrow(err, __.input, __.filename, __.lineno);
+buf.push('</p>');
 }
+return buf.join("");
 };
